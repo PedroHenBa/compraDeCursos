41d2316cc139fd30536092c962c04683b24916fc
PedroHenBa <pedroh.ba04@gmail.com>
2021-11-13 18:28:50 -0300

    Adicionado novas colunas na model curso

--- app/Models/CursosModel.php
+++ app/Models/CursosModel.php
@@ -6,10 +6,18 @@ class CursosModel extends Model
 {
     protected $table = "cursos";
     protected $primaryKey = "id";
-    protected $allowedFields = ["name", "nomeCategoria", "preco"];
+    protected $allowedFields = [
+        "name",
+        "nomeCategoria",
+        "preco",
+        "descricao",
+        "imagemUrl",
+    ];
     protected $validationRules = [
         "name" => "required|max_length[50]",
         "nomeCategoria" => "required|max_length[50]",
         "preco" => "required|decimal",
+        "descricao" => "required|max_length[255]",
+        "imagemUrl" => "max_length[255]",
     ];
 }
